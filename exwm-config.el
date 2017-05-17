759768dc6dd9979b44a761dbc1506bb87cfee5e1
Balletie <skip_meesie@hotmail.com>
2017-05-17 09:40:49 +0200

    Add keybindings to exwm config

--- exwm-config.el
+++ exwm-config.el
@@ -12,6 +12,9 @@
 	    (exwm-workspace-rename-buffer
 	     (truncate-string-to-width (format "%s : %s" exwm-class-name exwm-title)
 				       43 0 nil "..."))))
+
+;;;; Keybindings
+;;; Global keys
 ;; 's-r': Reset
 (exwm-input-set-key (kbd "s-r") #'exwm-reset)
 ;; 's-w': Switch workspace
@@ -32,6 +35,30 @@
                     (lambda ()
 		      (interactive)
 		      (start-process-shell-command "urxvtc" nil "urxvtc")))
+
+;;; Local keybindings (just in line-mode). Taken from
+;;; https://github.com/carbohydratesn/spacemacs/blob/deea9318ff806ff4e530e3fcce3ea829b0a76d45/layers/%2Bwindow-management/exwm/packages.el
+;; Press C-q to send key to X window.
+(push ?\C-q exwm-input-prefix-keys)
+(define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
+
+;; Universal Get-me-outta-here
+(push ?\C-g exwm-input-prefix-keys)
+;; Universal Arguments
+(push ?\C-u exwm-input-prefix-keys)
+(push ?\C-0 exwm-input-prefix-keys)
+(push ?\C-1 exwm-input-prefix-keys)
+(push ?\C-2 exwm-input-prefix-keys)
+(push ?\C-3 exwm-input-prefix-keys)
+(push ?\C-4 exwm-input-prefix-keys)
+(push ?\C-5 exwm-input-prefix-keys)
+(push ?\C-6 exwm-input-prefix-keys)
+(push ?\C-7 exwm-input-prefix-keys)
+(push ?\C-8 exwm-input-prefix-keys)
+(push ?\C-9 exwm-input-prefix-keys)
+;; C-c is needed for copying
+(delete ?\C-c exwm-input-prefix-keys)
+(delete ?\C-v exwm-input-prefix-keys)
 
 (require 'exwm-systemtray)
 (setq exwm-systemtray-height 15)
